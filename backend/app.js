b064d118ddb00099059f6231f924456022f94dce
Luu Vinh Khoi <luukhoi182@gmail.com>
2025-06-13 12:33:40 +0700

    Modify cors

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,10 @@ const sessionMiddleware=session({
 dotenv.config()
 app.use(sessionMiddleware)
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    'https://master.d2jsu35to5wcgl.amplifyapp.com/',
+    'http://localhost:5173', // Cho development
+  ],
   methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'], // Chỉ cho phép các phương thức này
   credentials: true, // Nếu cần gửi cookie hoặc thông tin xác thực
 }));
